d6aed3ecd71c464f40475ff1fd8b51afde478a82
aydin <timur.schmidt@student.maastrichtuniversity.nl>
2024-06-09 15:04:20 +0200

    Refactoring

--- src/test/java/data_management/WebSocketReaderTest.java
+++ src/test/java/data_management/WebSocketReaderTest.java
@@ -5,15 +5,13 @@ import com.data_management.WebSocketReader;
 import org.java_websocket.handshake.ServerHandshake;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import javax.xml.crypto.Data;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import static org.mockito.Mockito.*;
 
-public class WebSocketReaderTest {
+class WebSocketReaderTest {
     private DataStorage dataStorage;
     private WebSocketReader webSocketReader;
 
@@ -24,48 +22,48 @@ public class WebSocketReaderTest {
     }
 
     @Test
-    public void onOpen_shouldPrintConnectedToServer() {
+    void onOpen_shouldPrintConnectedToServer() {
         ServerHandshake serverHandshake = mock(ServerHandshake.class);
         webSocketReader.onOpen(serverHandshake);
         // Verify that "Connected to server" is printed to the console
     }
 
     @Test
-    public void onMessage_shouldCallReceiveData() {
+    void onMessage_shouldCallReceiveData() {
         webSocketReader.onMessage("1,1622544000,temperature,36.6");
         verify(dataStorage, times(1)).addPatientData(1, 36.6, "temperature", 1622544000);
     }
 
     @Test
-    public void onClose_shouldPrintDisconnectedFromServer() {
+    void onClose_shouldPrintDisconnectedFromServer() {
         webSocketReader.onClose(1000, "Normal closure", true);
         // Verify that "Disconnected from server" is printed to the console
     }
 
     @Test
-    public void onError_shouldPrintExceptionMessage() {
+    void onError_shouldPrintExceptionMessage() {
         Exception ex = new Exception("Test exception");
         webSocketReader.onError(ex);
         // Verify that "Exception! Test exception" is printed to the console
     }
 
     @Test
-    public void receiveData_shouldAddPatientData() {
+    void receiveData_shouldAddPatientData() {
         webSocketReader.recieveData("1,1622544000,temperature,36.6");
         verify(dataStorage, times(1)).addPatientData(1, 36.6, "temperature", 1622544000);
     }
 
     @Test
-    public void receiveData_shouldHandleInvalidData() {
+    void receiveData_shouldHandleInvalidData() {
         webSocketReader.recieveData("invalid data");
         verify(dataStorage, never()).addPatientData(anyInt(), anyDouble(), anyString(), anyLong());
     }
     @Test
-    public void receiveData_shouldNotAddIncompletePatientData() throws URISyntaxException {
-        DataStorage dataStorage = mock(DataStorage.class);
+    void receiveData_shouldNotAddIncompletePatientData() throws URISyntaxException {
+        DataStorage dataStorage1 = mock(DataStorage.class);
         WebSocketReader webSocketReader1 = new WebSocketReader(new URI("ws://localhost:8080"), dataStorage);
 
         webSocketReader1.recieveData("1,122332535, temperature");
-        verify(dataStorage, never()).addPatientData(anyInt(), anyDouble(), anyString(), anyLong());
+        verify(dataStorage1, never()).addPatientData(anyInt(), anyDouble(), anyString(), anyLong());
     }
 }
